d04789c5df117578d5281b41a413bcf122b5c7bc
kalyan751 <kalyan.tmsl@gmail.com>
2023-09-23 01:17:35 +0530

    datatypes

---
+++ 01_basics/L02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as newer version of js
+// alert(3 + 3) // we are using node js and not browser
+
+console.log(3+3); //code readability 
+console.log("kalyan");
+
+let name = "hitesh";
+let age = 18;
+let isLogged = true;
+let state = null;
+/* 
+
+number
+bigint
+string => ""
+boolean => true/false
+null  => standalone value
+undefined
+symbol => uniqueness
+
+object
+*/
+
+console.log(typeof(age))
+console.log(typeof(undefined))
+console.log(typeof null)
